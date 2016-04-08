87b7a05e8aa19ecb740d552631ce3b7ab0104f3a
Prasanna Gupta <prasanna.gupta@randem.co.in>
2016-04-08 12:37:52 +0530

    added nslog

--- ViewController.m
+++ ViewController.m
@@ -23,6 +23,7 @@
     FetchContacts *contactObj = [[FetchContacts alloc]init];
     arrContacts = [[contactObj fetchAllContacts] mutableCopy];
     
+    NSLog(@"arrContacts %zd",arrContacts.count);
     // Do any additional setup after loading the view, typically from a nib.
 }
 
